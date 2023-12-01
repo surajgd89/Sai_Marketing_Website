4f94dd06c28397a449e6907d761175f6f4bcca1c
Suraj Patil <suraj.gd89@gmail.com>
2023-12-01 16:03:09 +0530

    initial8

---
+++ js/main.js
@@ -0,0 +1,16 @@
+//RESPONSIVE MENU
+
+const menuToggleBtn = document.querySelector('.header__toggle');
+const headerNav = document.querySelector('.header__nav');
+
+menuToggleBtn.addEventListener('click', () => {
+   headerNav.classList.toggle('header__nav--toggle');
+});
+
+
+
+
+
+
+
+
